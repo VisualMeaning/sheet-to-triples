239f26c2c016946920a242cb4e414e0baf081f88
jjeezy <70636450+jjeezy@users.noreply.github.com>
2020-09-22 17:00:38 +0100

    Add HE internal stories transform (#11)
    Setting up tile mapping for story overlays.

---
+++ transforms/he_int_stories.py
@@ -0,0 +1,25 @@
+{
+    'data': [
+        ('01_internalmodel', 'Internal Model'),
+        ('02_keyquestions', 'Key Questions'),
+        ('03_interventions', 'Potential Interventions'),
+        ('04_stakeholders', 'Stakeholders'),
+        ('05_currentprojects', 'Current Projects'),
+    ],
+    'lets': {
+        'storyName': '{row[1]}',
+        'version': '20200922',
+    },
+    'queries': {
+        'story': (
+            'select ?s where {'
+            ' ?s rdf:type vm:Story .'
+            ' ?s vm:name ?storyName .'
+            '}'),
+    },
+    'triples': [
+        ('{story}', 'vm:usesMapTiles',
+            'https://opatlas-live.s3.amazonaws.com/highways_internal_model/'
+            '{version}/overlays/{row[0]}/{{z}}-{{x}}-{{y}}.png'),
+    ],
+}
