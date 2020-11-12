f5e1a38fdb565cf286d1fb2a908db58c89cd43ab
Martin Packman <martin.packman@visual-meaning.com>
2020-11-12 01:18:00 +0000

    Add first views for HE base map

---
+++ transforms/he_views.py
@@ -0,0 +1,18 @@
+{
+    'data': [
+        ('process', 'Process'),
+        ('org-structure', 'Org Structure'),
+    ],
+    'lets': {
+        'iri': 'vm:view-he-{row[0]}',
+        'version': '20201111',
+    },
+    'triples': [
+        ('{iri}', 'rdf:type', 'vm:View'),
+        ('{iri}', 'vm:name', '{row[1]}'),
+        ('{iri}', 'vm:usesMapTiles',
+            'https://opatlas-live.s3.amazonaws.com/hwtest/{version}/overlays/'
+            '{row[0]}/{{z}}-{{x}}-{{y}}.png'),
+        ('{iri}', 'vm:asOrdinal', '{n}'),
+    ],
+}
