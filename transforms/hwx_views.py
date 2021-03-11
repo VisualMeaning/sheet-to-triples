3111a31344341b653f10002f25e7d7691823a172
Martin Packman <martin.packman@visual-meaning.com>
2021-03-11 10:53:24 +0000

    Updates to HE transforms
    Some data in source sheet changed, and updated views.

--- transforms/hwx_views.py
+++ transforms/hwx_views.py
@@ -1,19 +1,21 @@
 {
     'data': [
-        ('HE/org', 'org', 'Organisation View', 'org', ''),
+        ('activity', '', 'Activity View', 'a=HE/Activity'),
+        ('org', 'org', 'Organisation View',
+            'a=org:OrganisationalUnit+foaf:Person'),
     ],
     'lets': {
         'iri': 'vm:{row[0]}',
-        'version': '20210308',
+        'version': '20210310',
     },
     'triples': [
         ('{iri}', 'rdf:type', 'vm:View'),
         ('{iri}', 'vm:name', '{row[2]}'),
         ('{iri}', 'vm:usesMapTiles',
             'https://opatlas-live.s3.amazonaws.com/hwx/{version}/overlays/'
-            '{row[1]}/{{z}}-{{x}}-{{y}}.png#background:#fff'),
-        ('{iri}', 'vm:useFilters', '{row[4]}'),
+            '{row[1].as_text}/{{z}}-{{x}}-{{y}}.png#background:#fff'),
+        ('{iri}', 'vm:useFilters', '{row[3]}'),
         ('{iri}', 'vm:asOrdinal', '{n}'),
-        ('{iri}', 'vm:comment', '{row[3]}'),
+        ('{iri}', 'vm:comment', '{row[0]}'),
     ],
 }
