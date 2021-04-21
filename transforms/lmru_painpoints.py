68c253cfd2239618a8240ef79c4c8f26250d863a
Martin Packman <martin.packman@visual-meaning.com>
2021-04-21 23:14:28 +0100

    New better aliasing and not-Stakeholder hacks

--- transforms/lmru_painpoints.py
+++ transforms/lmru_painpoints.py
@@ -12,8 +12,10 @@
         ('{iri}', 'rdf:type', 'vm:Painpoint'),
         ('{iri}', 'vm:harmsCapitalType', '{row[Capital type].as_capital}'),
         ('{iri}', 'vm:name', '{row[Painpoint / Bright spot title].as_text}'),
-        ('{iri}', 'vm:description', '{row[Painpoint / Bright spot description].as_text}'),
+        ('{iri}', 'vm:description',
+            '{row[Painpoint / Bright spot description].as_text}'),
         ('{iri}', 'vm:ofStakeholder', '{stakeholder}'),
+        ('{stakeholder}', 'rdf:type', 'vm:Stakeholder'),
         ('{iri}', 'vm:atGeoPoint', '{row[Coordinates].as_geo}'),
     ],
 }
--- transforms/lmru_stakeholders.py
+++ transforms/lmru_stakeholders.py
@@ -3,9 +3,8 @@
     'lets': {
         'iri': 'vm:_stakeholder_{row[Category / Stakeholder name].as_slug}',
     },
-    'non_unique': ['vm:comment', 'vm:name', 'vm:description'],
+    'non_unique': ['vm:name', 'vm:description'],
     'triples': [
-        ('{iri}', 'rdf:type', 'vm:Stakeholder'),
         ('{iri}', 'vm:name', '{row[Category / Stakeholder name].as_json}@en'),
         ('{iri}', 'vm:name',
             '{row[Russian Translation: Category / Stakeholder name].as_json}'
@@ -16,8 +15,7 @@
             '@ru'),
         ('{iri}', 'vm:broader',
             'vm:_stakeholder_{row[Parent Grouping].as_slug}'),
-        ('{iri}', 'vm:comment', '{row[Map label name].as_json}@en'),
-        ('{iri}', 'vm:comment',
-            '{row[Russian translation: label name].as_json}@ru'),
+        ('{iri}', 'owl:sameAs',
+            'vm:_stakeholder_{row[Map label name (if different)].as_slug}'),
     ],
 }
