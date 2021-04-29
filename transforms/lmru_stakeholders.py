ce0c9635457cbf92c4377660676177f61610cd5b
Martin Packman <martin.packman@visual-meaning.com>
2021-04-29 23:02:30 +0100

    LM transforms back to multilanguage form
    Also make everything that should be a stakeholder again.

--- transforms/lmru_stakeholders.py
+++ transforms/lmru_stakeholders.py
@@ -5,13 +5,14 @@
     },
     'non_unique': ['vm:name', 'vm:description'],
     'triples': [
+        ('{iri}', 'rdf:type', 'vm:Stakeholder'),
         ('{iri}', 'vm:name', '{row[Category / Stakeholder name].as_json}@en'),
         ('{iri}', 'vm:name',
             '{row[Russian Translation: Category / Stakeholder name].as_json}'
             '@ru'),
         ('{iri}', 'vm:description', '{row[Description].as_json}@en'),
         ('{iri}', 'vm:description',
-            '{row[Russian Translation: Category / Stakeholder name].as_json}'
+            '{row[Russian translation: description].as_json}'
             '@ru'),
         ('{iri}', 'vm:broader',
             'vm:_stakeholder_{row[Parent Grouping].as_slug}'),
