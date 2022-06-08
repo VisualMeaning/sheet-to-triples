3c7fc156177704c49480467999a26423aa8d12b0
alightwing <34476179+alightwing@users.noreply.github.com>
2022-06-08 12:48:39 +0100

    Fix linting bug (#112)

--- sheet_to_triples/tests/test_main.py
+++ sheet_to_triples/tests/test_main.py
@@ -286,4 +286,7 @@ class TestMain(fake_filesystem_unittest.TestCase):
 
         runner = run.Runner.from_args(args)
         main.run_runner(runner, args)
-        self.assertEqual(runner.non_unique, non_uniques.union(run._default_non_unique))
+        self.assertEqual(
+            runner.non_unique,
+            non_uniques.union(run._default_non_unique),
+        )
